828e331b73ded1b7fdee2bd9f9674da0d811d50f
Prasads33 <sangpal.prasad4@gmail.com>
2018-10-10 09:23:53 +0530

    Update GetRest/index.js

--- GetRest/index.js
+++ GetRest/index.js
@@ -1,11 +1,37 @@
-var request = require("request");
+var express = require('express');
+var assert= require('assert');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = require('should');
 
-request({
-    uri: "https://jsonplaceholder.typicode.com/posts/1",
-    method: "GET",
-    timeout: 10000,
-    /*followRedirect: true,
-    maxRedirects: 10*/
-}, function(error, response, body) {
-    console.log(body);
+
+
+
+
+describe('Response', function() {
+
+    it('should list items on GET', function (done) {
+        chai.request(app)
+            .get('https://jsonplaceholder.typicode.com/posts/1')
+            .end(function (err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body[0].should.be.a('object');
+                res.body[0].should.have.property('userId');
+                res.body[0].should.have.property('title');
+                res.body[0].userId.should.be.a('1');
+                res.body[0].title.should.equal('sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
+                done();
+            });
+    });
+
+
+
+
 });
+
+
+
+
